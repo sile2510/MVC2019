17cd5309120268d9c04b4f86e3678a71ce197b18
sile2510 <sile2510>
2019-03-28 18:50:03 +0100

    kreiranje kontrolera

---
+++ HelloWorld/Controllers/KreiranjeKontroleraController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class KreiranjeKontroleraController : Controller
+    {
+        // GET: KreiranjeKontrolera
+        public string Index()
+        {
+            string poruka = "Danas je: " + DateTime.Now.ToLongDateString();
+            return poruka;
+        }
+
+        public string QueryPodaci()
+        {
+            if (Request.QueryString["Ime"]==null || Request.QueryString["Prezime"] == null)
+            {
+                return "Podaci su nepotpuni!";
+            }
+            else
+            {
+                string ime = Request.QueryString["Ime"];
+                string prezime = Request.QueryString["Prezime"];
+                string tekstSaStilom = string.Format
+                    ("<lable style='color:red; font-weight:bold; '>{0} {1}</label>", ime, prezime);
+                return string.Format("<p>Ime i prezime iz query stringa su: {0}</p>", tekstSaStilom);
+                     
+            }
+            
+            
+            
+        }
+        public string RoutePodaci()
+        {
+            try
+            {
+                string kontroler = RouteData.Values["controller"].ToString();
+                string akcijskaMetoda = RouteData.Values["action"].ToString();
+                string parametarId = RouteData.Values["id"].ToString();
+                return "<h1>Route podaci:</h1>" +
+                    "Kontroler: " + kontroler + "<br/>" +
+                    "Metoda: " + akcijskaMetoda + "<br/>" +
+                    "Parametar id: " + parametarId + "<br/>";
+            }
+            catch(Exception e)
+            {
+                return "Došlo je do pogreške: " + e.Message;
+            }
+        }
+    }
+}
---
+++ HelloWorld/Controllers/Zbirka1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class Zbirka1Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            string message = "Rezultat operacije 4 + 3 * 3 = ";
+            return View((object)message);
+        }
+    }
+}
