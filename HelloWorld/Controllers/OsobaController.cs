33db370deaa63ad591094cecb90d8038bc14af6e
sile2510 <sile2510>
2019-03-29 18:40:24 +0100

    models

---
+++ HelloWorld/Controllers/OsobaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Controllers;
+using HelloWorld.Models;
+
+
+
+namespace HelloWorld.Controllers
+{
+    public class OsobaController : Controller
+    {
+        // GET: Osobe
+        public ViewResult PopuniOsobu()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult PrikaziOsobu (Osoba osoba)
+        {
+            return View(osoba);
+        }
+    }
+}
---
+++ HelloWorld/Controllers/SimpleBindingController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class SimpleBindingController : Controller
+    {
+        // GET: SimpleBinding
+        public ViewResult SimpleBindMetoda()
+        {
+            return View("SimpleBindMetoda");
+        }
+        [HttpPost]
+        public ViewResult SimpleBindMetoda(string ime)
+        {
+            string pozdrav = "Pozdrav, " + ime + "!";
+            return View("SimpleBindMetoda", (object)pozdrav);
+                
+        }
+    }
+}
