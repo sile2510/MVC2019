89d72a3bd7a10482ef53964dece8b537d11f9701
sile2510 <sile2510>
2019-04-01 18:22:25 +0200

    zadaca 2/3

---
+++ HelloWorld/Controllers/TocanOdgovorController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class TocanOdgovorController : Controller
+    {
+        // GET: TocanOdgovor
+        public ViewResult ProvjeriOdgovor()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult ProvjeriOdgovor(string odgovor)
+        {
+            string rezultat;
+            if (!string.IsNullOrEmpty(odgovor))
+            {
+
+
+                if (odgovor == "Bugarska")
+                {
+                    rezultat = "Odgovor je točan";
+                    return View((object)rezultat);
+                }
+                else
+                {
+                    rezultat = "Odgovor nije točan";
+                    return View((object)rezultat);
+                }
+
+            }
+            else
+            {
+                rezultat = "Odgovor ne postoji";
+                return View((object)rezultat);
+
+            }
+        } 
+    }
+}
