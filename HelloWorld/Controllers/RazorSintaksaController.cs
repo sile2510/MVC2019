4a00e969e6a74318380a661dbb4ca8f42b714874
sile2510 <sile2510>
2019-04-02 16:28:18 +0200

    razor

---
+++ HelloWorld/Controllers/RazorSintaksaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class RazorSintaksaController : Controller
+    {
+        // GET: RazorSintaksa
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult ViewZaForPetlju()
+        {
+            string[] voce = new string[]
+            {
+                "Jabuka",
+                "Kruska",
+                "Limun",
+                "Banana",
+                "Šljiva"
+            };
+            return View(voce);
+        }
+    }
+}
