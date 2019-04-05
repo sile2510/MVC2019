a277def59358f8b5715c53457108339cf8eec041
sile2510 <sile2510>
2019-04-05 17:33:12 +0200

    VALIDACIJE

---
+++ validacije1/Controllers/ValidacijeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using validacije1.Models;
+
+namespace validacije1.Controllers
+{
+    public class ValidacijeController : Controller
+    {
+        // GET: Validacije
+        public ViewResult IzdavanjeRacuna()
+        {
+            return View(new Racun()
+            {
+                Datum = DateTime.Now,
+                BrojRacuna = "/" +
+                DateTime.Now.Year.ToString()
+            });
+           
+        }
+        [HttpPost]
+        public ViewResult IzdavanjeRacuna(Racun racun)
+        {
+            if (string.IsNullOrEmpty(racun.BrojRacuna))
+            {
+                ModelState.AddModelError("BrojRacuna", "Broj računa je obavezan!");
+            }
+            if (string.IsNullOrEmpty(racun.Zaposlenik))
+            {
+                ModelState.AddModelError("Zaposlenik", "Zaposlenik je obavezan!");
+            }
+            if (string.IsNullOrEmpty(racun.Kupac))
+            {
+                ModelState.AddModelError("Kupac", "Kupac je obavezan!");
+            }
+            if (ModelState.IsValidField("Datum"))
+            {
+                if (racun.Datum < DateTime.Today.AddDays(-3))
+                {
+                    ModelState.AddModelError("", "Datum ne smije biti manji za više od 3 dana!");
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                return View("RacunIzdan", racun);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ validacije1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="validacije1.MvcApplication" Language="C#" %>
