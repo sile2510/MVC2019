56b3ed139b0431c8009f5e82d59bdf0f1933d2fe
sile2510 <sile2510>
2019-04-09 16:07:04 +0200

    novo

---
+++ spajanje_na_bazu/Controllers/SqlConnectionObjectController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace spajanje_na_bazu.Controllers
+{
+    public class SqlConnectionObjectController : Controller
+    {
+        // GET: SqlConnectionObject
+        public ActionResult Index()
+        {
+            //Prvo kreiramo conn string
+            string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+            // Nakon toga instanca SQLconnection
+            SqlConnection conn = new SqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    Response.Write("Konekcija je uspjela");
+                }
+            }
+            catch (SqlException sqex)
+            {
+                Response.Write("Greška spajanja na bazu: " + sqex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Neka greška: " + ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View();
+        }
+    }
+}
---
+++ spajanje_na_bazu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="spajanje_na_bazu.MvcApplication" Language="C#" %>
